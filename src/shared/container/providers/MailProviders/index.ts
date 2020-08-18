cae6d4ffd27dcad98d327c33e9b7724876440b75
Filubil <denison.shenry@gmail.com>
2020-08-18 10:15:04 -0300

    Adicionado o serviço de SES da AWS

--- src/shared/container/providers/MailProviders/index.ts
+++ src/shared/container/providers/MailProviders/index.ts
@@ -4,9 +4,11 @@ import mailConfig from '@config/mail';
 
 import IMailProvider from './models/IMailProvider';
 import EtherealMailProvider from './implementations/EtherealMailProvider';
+import AWSSESMailProvider from './implementations/AWSSESMailProvider';
 
 const provider = {
   ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(AWSSESMailProvider),
 };
 
 container.registerInstance<IMailProvider>(
